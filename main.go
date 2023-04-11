4371930da7a3eb455285779520a4736f51cab9be
pablozuta <pablo.zuta.h@gmail.com>
2023-04-11 01:13:06 -0400

    se modifica main.go

--- main.go
+++ main.go
@@ -11,8 +11,9 @@ func main() {
 	fs := http.FileServer(http.Dir("static"))
 
 	// se usa el metodo StripPrefix para servir index.html como default en /
-	http.Handle("/", http.StripPrefix("/", fs))
-	
+	home := http.FileServer(http.Dir("/index.html"))
+	http.Handle("/", http.StripPrefix("/", home))
+
 	// iniciar servidor
 	log.Println("escuchando en puerto 8080:")
 	err := http.ListenAndServe(":8080", nil)
