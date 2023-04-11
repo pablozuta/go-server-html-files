618ab9d09775b84b8608c5be560963d3bff83f0a
pablozuta <pablo.zuta.h@gmail.com>
2023-04-11 01:07:58 -0400

    se modifica main.go para servir pagina home sin agregar index a url

--- main.go
+++ main.go
@@ -11,10 +11,8 @@ func main() {
 	fs := http.FileServer(http.Dir("static"))
 
 	// se usa el metodo StripPrefix para servir index.html como default en /
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "index.html")
-	})
-
+	http.Handle("/", http.StripPrefix("/", fs))
+	
 	// iniciar servidor
 	log.Println("escuchando en puerto 8080:")
 	err := http.ListenAndServe(":8080", nil)
